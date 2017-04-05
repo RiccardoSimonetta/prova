2a6c05bb48eaae00d5e81353f7f6b7285aabf7e6
RiccardoSimonetta <wicky88@hotmail.it>
2017-04-05 17:44:52 +0200

    commit iniziale prova_github_project

---
+++ prova_github_project/src/prova_github_project/Main.java
@@ -0,0 +1,10 @@
+package prova_github_project;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
